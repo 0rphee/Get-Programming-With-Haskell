0bfaeaedc320950cb974ff1cc4df48da823798a8
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2021-12-27 14:43:42 -0600

    udpate

--- Lesson5.hs
+++ Lesson5.hs
@@ -11,4 +11,10 @@ getHostRequestBuilder host = (\apiKey resource id ->
 
 exampleUrlBuilder = getHostRequestBuilder "https://example.com"
 
-genApiRequestBuilder hostBuilder apiKey = (/resource id -> hostBuilder apiKey resource id)
+genApiRequestBuilder hostBuilder apiKey = (\resource id -> hostBuilder apiKey resource id)
+
+myExampleUrlBuilder = genApiRequestBuilder exampleUrlBuilder "12321hask23ll"
+
+-- 5.2.1 Partial application: making closures simple
+
+add4 a b c d = a + b + c + d
