fe25f844eac9ba86a789371cbdff2005c005fda5
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2021-12-27 15:38:54 -0600

    Update Lesson5.hs

--- Lesson5.hs
+++ Lesson5.hs
@@ -9,12 +9,22 @@ getRequestUrl host key resource id = host ++
 getHostRequestBuilder host = (\apiKey resource id ->
                                getRequestUrl host apiKey resource id)
 
+getHostRequestBuilder2 host = (\apiKey resource id -> getRequestUrl host apiKey resource id)
+
 exampleUrlBuilder = getHostRequestBuilder "https://example.com"
+
+-- same function but built with partial application
+exampleUrlBuilder2 = getRequestUrl "https://example.com"
 
 genApiRequestBuilder hostBuilder apiKey = (\resource id -> hostBuilder apiKey resource id)
 
 myExampleUrlBuilder = genApiRequestBuilder exampleUrlBuilder "12321hask23ll"
+-- same function but built with partial application
+exampleUrlWithKeyBuilder = exampleUrlBuilder2 "myApiKey"
+-- Quick check 5.3
+finalExampleUrlBuilder = getRequestUrl "https://example.com" "1337hAsk3ll" "book"
+
 
 -- 5.2.1 Partial application: making closures simple
-
 add4 a b c d = a + b + c + d
+
