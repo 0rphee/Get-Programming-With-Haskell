30eb9bb9d83b244bbbbef953a111a71c29fff1a6
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2022-04-09 14:32:54 -0500

    finished l21

--- U4/L21/pizza.hs
+++ U4/L21/pizza.hs
@@ -1,30 +1,40 @@
-pizzaCost :: Fractional a => a -> a -> a
-pizzaCost size cost = size / cost
+type Pizza = (Double, Double)
 
-assembleTxt :: (Fractional a, Show a) => a -> a -> String
-assembleTxt size costInch = "The "++sizeStr++" pizza is cheaper at "++costInchStr++" per square inch"
+areaGivenDiameter :: Double -> Double
+areaGivenDiameter size = pi*(size/2)^2
+
+costPerInch :: Pizza -> Double
+costPerInch (size, cost) = cost / areaGivenDiameter size
+
+comparePizzas :: Pizza -> Pizza -> Pizza
+comparePizzas p1 p2 = if costP1 < costP2
+                      then p1
+                      else p2
+    where   costP1 = costPerInch p1
+            costP2 = costPerInch p2
+
+assembleTxt :: Pizza -> String
+assembleTxt (size,cost) = "The "++sizeStr++"-inch pizza is cheaper at "
+                                 ++costInchStr++" per square inch"
     where sizeStr = show size
-          costInchStr = show costInch
+          costInchStr = show $ costPerInch (size,cost)
 
-getFractional :: IO a 
+getFractional :: IO Double
 getFractional = do
-    num <- getLine 
+    num <- getLine
     return (read num)
 
-askPizzaValues :: IO (a, a)
-askPizzaValues = do
-    putStrLn "What is the size of the pizza"
+askPizzaValues :: (Num a,Show a) => a -> IO Pizza
+askPizzaValues pizzaNum = do
+    putStrLn ("What is the size of the pizza " ++ show pizzaNum)
     size <- getFractional
-    putStrLn "What is the cost of the pizza"
+    putStrLn ("What is the cost of the pizza " ++ show pizzaNum)
     cost <- getFractional
     return (size, cost)
 
-
 main :: IO ()
 main = do
-    pair <-  askPizzaValues
-    let size = fst pair
-    let cost = snd pair 
-    let costInch = pizzaCost size cost
-    let finalTxt = assembleTxt size costInch
-    putStrLn finalTxt
+    p1 <- askPizzaValues 1 
+    p2 <- askPizzaValues 2
+    let finalPizza = comparePizzas p1 p2
+    putStrLn $ assembleTxt finalPizza
